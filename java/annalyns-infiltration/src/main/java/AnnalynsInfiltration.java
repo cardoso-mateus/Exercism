10e2f9327eb411dd247bba8dd18db268491ef60a
Mateus Cardoso <90119399+cardoso-mateus@users.noreply.github.com>
2024-06-14 03:48:47 -0300

    Primeiro exercício booleano concluído

--- java/annalyns-infiltration/src/main/java/AnnalynsInfiltration.java
+++ java/annalyns-infiltration/src/main/java/AnnalynsInfiltration.java
@@ -1,17 +1,34 @@
 class AnnalynsInfiltration {
     public static boolean canFastAttack(boolean knightIsAwake) {
-        throw new UnsupportedOperationException("Please implement the (static) AnnalynsInfiltration.canFastAttack() method");
+        if (knightIsAwake) {
+            return false;
+        }
+        return true;
     }
 
     public static boolean canSpy(boolean knightIsAwake, boolean archerIsAwake, boolean prisonerIsAwake) {
-        throw new UnsupportedOperationException("Please implement the (static) AnnalynsInfiltration.canSpy() method");
+//      In all case scenarios, that is the only one sapying can fail, so there is no need for multiples checks
+        if (!knightIsAwake && !archerIsAwake && !prisonerIsAwake) {
+            return false;
+        }
+        return true;
     }
 
     public static boolean canSignalPrisoner(boolean archerIsAwake, boolean prisonerIsAwake) {
-        throw new UnsupportedOperationException("Please implement the (static) AnnalynsInfiltration.canSignalPrisoner() method");
+//      That is the only case scenario when signaling can work so no more checks are needed also
+        if (!archerIsAwake && prisonerIsAwake) {
+            return true;
+        }
+        return false;
     }
 
     public static boolean canFreePrisoner(boolean knightIsAwake, boolean archerIsAwake, boolean prisonerIsAwake, boolean petDogIsPresent) {
-        throw new UnsupportedOperationException("Please implement the (static) AnnalynsInfiltration.canFreePrisoner() method");
+        if (!knightIsAwake && !archerIsAwake && prisonerIsAwake && !petDogIsPresent){
+            return true;
+//      That is the only case scenario where the method can fail but must be check second, otherwise the test will always fail
+        } else if (archerIsAwake || !petDogIsPresent) {
+            return false;
+        }
+        return true;
     }
 }
