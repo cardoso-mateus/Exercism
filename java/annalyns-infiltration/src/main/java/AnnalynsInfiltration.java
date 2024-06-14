70b4054779ffe865a88809e6b6cc99354cf3d7f8
Mateus Cardoso <90119399+cardoso-mateus@users.noreply.github.com>
2024-06-14 04:28:20 -0300

    Primeiro exercício booleano concluído segunda iteração

--- java/annalyns-infiltration/src/main/java/AnnalynsInfiltration.java
+++ java/annalyns-infiltration/src/main/java/AnnalynsInfiltration.java
@@ -1,34 +1,17 @@
 class AnnalynsInfiltration {
     public static boolean canFastAttack(boolean knightIsAwake) {
-        if (knightIsAwake) {
-            return false;
-        }
-        return true;
+        return !knightIsAwake;
     }
 
     public static boolean canSpy(boolean knightIsAwake, boolean archerIsAwake, boolean prisonerIsAwake) {
-//      In all case scenarios, that is the only one sapying can fail, so there is no need for multiples checks
-        if (!knightIsAwake && !archerIsAwake && !prisonerIsAwake) {
-            return false;
-        }
-        return true;
+        return knightIsAwake || archerIsAwake || prisonerIsAwake;
     }
 
     public static boolean canSignalPrisoner(boolean archerIsAwake, boolean prisonerIsAwake) {
-//      That is the only case scenario when signaling can work so no more checks are needed also
-        if (!archerIsAwake && prisonerIsAwake) {
-            return true;
-        }
-        return false;
+        return !archerIsAwake && prisonerIsAwake;
     }
 
     public static boolean canFreePrisoner(boolean knightIsAwake, boolean archerIsAwake, boolean prisonerIsAwake, boolean petDogIsPresent) {
-        if (!knightIsAwake && !archerIsAwake && prisonerIsAwake && !petDogIsPresent){
-            return true;
-//      That is the only case scenario where the method can fail but must be check second, otherwise the test will always fail
-        } else if (archerIsAwake || !petDogIsPresent) {
-            return false;
-        }
-        return true;
+        return (!archerIsAwake && petDogIsPresent) || (!knightIsAwake && !archerIsAwake && prisonerIsAwake);
     }
 }
