2c4da16f944abea4a1528b4d20785510fff71266
Mateus Cardoso <90119399+cardoso-mateus@users.noreply.github.com>
2024-06-20 18:16:23 -0300

    Primeiro exercício números concluído

--- java/cars-assemble/src/main/java/CarsAssemble.java
+++ java/cars-assemble/src/main/java/CarsAssemble.java
@@ -1,10 +1,20 @@
 public class CarsAssemble {
 
     public double productionRatePerHour(int speed) {
-        throw new UnsupportedOperationException("Please implement the CarsAssemble.productionRatePerHour() method");
+        double productionRate;
+        if (speed > 9) {
+            productionRate = speed * (221 * 0.77);
+        } else if (speed > 8) {
+            productionRate = speed * (221 * 0.8);
+        } else if (speed > 4) {
+            productionRate = speed * (221 * 0.9);
+        } else {
+            productionRate = speed * 221;
+        }
+        return productionRate;
     }
 
     public int workingItemsPerMinute(int speed) {
-        throw new UnsupportedOperationException("Please implement the CarsAssemble.workingItemsPerMinute() method");
+        return (int) productionRatePerHour(speed) / 60;
     }
 }
