70c6d51d32c0f7df1d8dde524635801e7ae8220e
Mateus Cardoso <90119399+cardoso-mateus@users.noreply.github.com>
2024-06-20 18:36:34 -0300

    Primeiro exercício números concluído segunda iteração

--- java/cars-assemble/src/main/java/CarsAssemble.java
+++ java/cars-assemble/src/main/java/CarsAssemble.java
@@ -1,20 +1,20 @@
 public class CarsAssemble {
 
     public double productionRatePerHour(int speed) {
-        double productionRate;
-        if (speed > 9) {
-            productionRate = speed * (221 * 0.77);
-        } else if (speed > 8) {
-            productionRate = speed * (221 * 0.8);
-        } else if (speed > 4) {
-            productionRate = speed * (221 * 0.9);
-        } else {
-            productionRate = speed * 221;
-        }
-        return productionRate;
+        double minimumProductionRate = 221;
+        return minimumProductionRate * speed * successRate(speed);
     }
 
     public int workingItemsPerMinute(int speed) {
         return (int) productionRatePerHour(speed) / 60;
+    }
+
+    private double successRate(int speed) {
+        double rate;
+        if (speed > 9) {rate = 0.77;}
+        else if (speed > 8) {rate = 0.8;}
+        else if (speed > 4) {rate = 0.9;}
+        else {rate = 1;}
+        return rate;
     }
 }
