f15a87bf99760450f8f80fd13a63275b32819c94
Mateus Cardoso <90119399+cardoso-mateus@users.noreply.github.com>
2024-06-29 15:55:18 -0300

    Primeiro exercício chars concluído

--- java/squeaky-clean/src/main/java/SqueakyClean.java
+++ java/squeaky-clean/src/main/java/SqueakyClean.java
@@ -1,5 +1,30 @@
-class SqueakyClean {
-    static String clean(String identifier) {
-        throw new UnsupportedOperationException("Please implement the (static) SqueakyClean.clean() method");
+    class SqueakyClean {
+        static String clean(String identifier) {
+            StringBuilder cleanString = new StringBuilder();
+            for (char character : identifier.toCharArray()) {
+                String string = String.valueOf(character);
+                if (string.matches("[^a-zA-Z0-9 -]")) {
+                    string = string.replace(string, "");
+                }
+                if (string.matches("[0-9]")) {
+                    string = string.replace("0", "o")
+                            .replace("1", "l")
+                            .replace("3", "e")
+                            .replace("4", "a")
+                            .replace("7", "t");
+                }
+                if (string.matches(" ")) {
+                    string = string.replace(string, "_");
+                }
+                cleanString.append(string);
+            }
+            String[] words = cleanString.toString().split("-");
+            StringBuilder camelCase = new StringBuilder(words[0]);
+            for (int i = 1; i < words.length; i++) {
+                String word = words[i];
+                camelCase.append(word.substring(0, 1).toUpperCase());
+                camelCase.append(word.substring(1));
+            }
+            return camelCase.toString();
+        }
     }
-}
