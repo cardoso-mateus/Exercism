d0685b129f82dda4245fe75fa63c721a22cd4f97
Mateus Cardoso <90119399+cardoso-mateus@users.noreply.github.com>
2024-07-02 22:50:14 -0300

    Primeiro exercício operadores ternários concluído segunda iteração

--- java/salary-calculator/src/main/java/SalaryCalculator.java
+++ java/salary-calculator/src/main/java/SalaryCalculator.java
@@ -1,21 +1,18 @@
 public class SalaryCalculator {
     public double salaryMultiplier(int daysSkipped) {
-        boolean applyPenalty = daysSkipped > 4;
-        return applyPenalty ? 0.85 : 1;
+        return daysSkipped > 4 ? 0.85 : 1;
     }
 
     public int bonusMultiplier(int productsSold) {
-        boolean applyBonus = productsSold > 19;
-        return applyBonus ? 13 : 10;
+        return productsSold > 19 ? 13 : 10;
     }
 
     public double bonusForProductsSold(int productsSold) {
-        return bonusMultiplier(productsSold) * productsSold;
+            return bonusMultiplier(productsSold) * productsSold;
     }
 
     public double finalSalary(int daysSkipped, int productsSold) {
         double salary = (1000 + bonusForProductsSold(productsSold)) * salaryMultiplier(daysSkipped);
-        boolean isSalaryCapped = salary > 2000;
-        return isSalaryCapped ? 2000 : salary;
+        return salary > 2000 ? 2000 : salary;
     }
 }
